697d9188a4a0385eae96d43881408b8bc261eb51
Nicola Aitken <naitken22@gmail.com>
2014-07-14 17:48:05 +0100

    grid testing

---
+++ lib/grid_test.rb
@@ -0,0 +1,23 @@
+user_input = "A1"
+
+shoot_to(user_input)
+
+take_shot(board(user_input))
+
+def take_shot(board_position)
+
+end
+
+
+if board[A1] == :des
+	board[A1]= value.hit
+else 
+	board[A1]= :miss
+end
+return display.board
+
+board = { A1: :des, A2: :des, B1: ' ', B2: ' '}
+
+des.destroyer
+
+destroyer = 
