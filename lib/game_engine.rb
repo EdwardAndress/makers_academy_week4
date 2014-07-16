8556e34b262310f17324e41e76ee6acfb1f639ec
Nicola Aitken <naitken22@gmail.com>
2014-07-16 19:47:07 +0100

    gam engine lets user place boat

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -8,7 +8,15 @@ class Game
 		@player2 = Player.new
 	end
 
-	def prompt_for_boat_deployment
-		puts "Where would you like to position #{@fleet.first}"
+	def prompt_for_boat_deployment(player)
+		puts "Where would you like to position #{player.fleet.first}"
+		get_coordinates(player)
 	end
+
+	def get_coordinates(player)
+		row = gets.chomp.to_i
+		col = gets.chomp.to_i
+		player.grid.deploy(player.fleet.shift, row, col, "vertical")
+	end
+
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,12 @@
 require_relative 'grid'
 
-require 'ship'
+require_relative 'ship'
 
 class Player
 
 	attr_reader :grid, :fleet
 
-	def initialize(grid)
+	def initialize(grid=Grid.new)
 			@grid = grid
 			@fleet=[submarine = Submarine.new, cruiser = Cruiser.new, aircraft_carrier = Aircraft_carrier.new, battleship = Battleship.new, destroyer = Destroyer.new]
 
